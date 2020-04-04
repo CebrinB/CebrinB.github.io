e8b71abbd93abcf03a08dd19501bafbcfc36a342
CebrinB <cebrin@byui.edu>
2020-04-03 22:48:17 -0500

    ..

--- Horse/main.js
+++ Horse/main.js
@@ -1,7 +1,7 @@
 import Horse from "./Horse.js";
 import User from "./User.js";
 
-const myHorse = new Horse(document.getElementById('horses'));
+const myHorse = new Horse(document.querySelector('#horses'));
 const user = new User();
 
 
@@ -15,28 +15,35 @@ if (localStorage.hasOwnProperty('horseList')) {
 });
 
 //set a listener for the user to open the menu
-document.getElementById('menuButton').addEventListener("click", () => {
+document.querySelector('#menuButton').addEventListener("click", () => {
   document.querySelector('#menu').classList.toggle('show');
   document.querySelector('#menuButton').classList.toggle('active');
 });
 
 //set a listener for the user to add a new horse
-document.getElementById('addButton').addEventListener("click", () => {
+document.querySelector('#addButton').addEventListener("click", () => {
   document.querySelector('#makeHorse').classList.toggle('show');
   document.querySelector('#main').classList.toggle('show');  
   document.querySelector('#rot').classList.toggle('cancel');
 });
 
-document.getElementById('submitForm').addEventListener('click', () => {
+document.querySelector('#add').addEventListener('click', () => {
+  document.querySelector('#makeHorse').classList.toggle('show');
+  document.querySelector('#menu').classList.toggle('show');
+  document.querySelector('#main').classList.toggle('show');  
+  document.querySelector('#rot').classList.toggle('cancel');
+});
+
+document.querySelector('#submitForm').addEventListener('click', () => {
   myHorse.addHorse();
 });
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.querySelector("#mySidenav").style.width = "250px";
+  document.querySelector("#main").style.marginLeft = "250px";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.querySelector("#mySidenav").style.width = "0";
+  document.querySelector("#main").style.marginLeft= "0";
 }
